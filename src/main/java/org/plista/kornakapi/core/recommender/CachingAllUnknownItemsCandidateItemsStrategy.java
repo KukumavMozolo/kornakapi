b6d19e0521280332ed80761057269581be2415fa
mw <mw@plista.com>
2015-01-23 18:10:06 +0100

    logging

--- src/main/java/org/plista/kornakapi/core/recommender/CachingAllUnknownItemsCandidateItemsStrategy.java
+++ src/main/java/org/plista/kornakapi/core/recommender/CachingAllUnknownItemsCandidateItemsStrategy.java
@@ -44,7 +44,13 @@ public class CachingAllUnknownItemsCandidateItemsStrategy implements CandidateIt
   public FastIDSet getCandidateItems(long userID, PreferenceArray preferencesFromUser, DataModel dataModel)
       throws TasteException {
     FastIDSet possibleItemIDs = allItemIDs.clone();
+      if(log.isInfoEnabled()){
+          log.info("PossibleItemIDs: {}", possibleItemIDs.toString());
+      }
     possibleItemIDs.removeAll(preferencesFromUser.getIDs());
+    if(log.isInfoEnabled()){
+        log.info("PossibleItemIDsRemoved: {}", possibleItemIDs.toString());
+    }
     return possibleItemIDs;
   }
 
